0470fde4be2c8869c3f0ccfa188c05855040688b
mila-carlton <mila.carlton@mail.ru>
2023-11-15 22:31:12 +0400

    coctails

---
+++ TestJSON/CoctailCell.swift
@@ -0,0 +1,23 @@
+//
+//  CoctailCell.swift
+//  TestJSON
+//
+//  Created by PASGON TEXTILE on 14.11.23.
+//
+
+import UIKit
+
+class CoctailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TestJSON/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  TestJSON
+//
+//  Created by PASGON TEXTILE on 14.11.23.
+//
+
+import Foundation
